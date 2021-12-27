c038c934900d0cfd50d9e76a66ce8bc3fa752327
Abel Mariano <242.ams@gmail.com>
2021-12-27 19:16:15 -0300

    Subida de Proyecto-JavaScript

---
+++ ingreso.js
@@ -0,0 +1,12 @@
+let mayoriaEdad = "¡Acepta sólo si eres mayor de 18 años!"
+
+confirm(mayoriaEdad)
+
+let ingresarAño = prompt ("Ingresa el año en que naciste:")
+
+if (ingresarAño >= 2004) {
+    alert("Tu no puedes acceder al sitio");}
+
+else if (ingresarAño <=2003) {
+    alert("Eres Mayor: ¡PUEDES TOMAR NUESTRA PINTA!");
+}
